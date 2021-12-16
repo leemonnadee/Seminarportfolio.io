27105be81e07f5f80c2b73ad5c10a85e7c9fbff0
leemonnadee <78067372+leemonnadee@users.noreply.github.com>
2021-12-16 21:14:54 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var typed = new Typed('.type', {
+    strings: ['Developer', 
+    'Designer',
+    'Freelancer',
+    'Student'
+],
+    typeSpeed:60,
+    backSpeed:60,
+    loop: true,
+   
+  });
+  
